3b9a8e2b37f7bcb3574a12168f15be383b2181c2
HammadSherani <hammadk426623@gmail.com>
2026-06-01 05:05:19 +0500

    fix: fix import path in app.js

--- apps/auth-service/src/server.ts
+++ apps/auth-service/src/server.ts
@@ -1,6 +1,6 @@
 
 
-import { buildApp } from './app';
+import { buildApp } from './app.js';
 import { env } from './config/env';
 
 async function start() {
--- apps/merchant-service/src/server.ts
+++ apps/merchant-service/src/server.ts
@@ -1,4 +1,4 @@
-import { buildApp } from './app';
+import { buildApp } from './app.js';
 import { env } from './config/env';
 
 async function start() {
