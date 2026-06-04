6dcddcdaccd6bedaf46b2d5961ac37923c568056
HammadSherani <hammadk426623@gmail.com>
2026-06-04 07:35:04 +0500

    port changes new

--- apps/api-gateway/src/server.ts
+++ apps/api-gateway/src/server.ts
@@ -1,9 +1,13 @@
 import { buildApp } from './app.js';
 
 async function start() {
+  const PORT = Number(process.env.PORT);
+
   const app = await buildApp();
-  await app.listen({ port: 3010, host: '0.0.0.0' });
-  console.log('🚀 API Gateway on port 3010');
+
+  await app.listen({ port: PORT, host: '0.0.0.0' });
+
+  console.log(`🚀 API Gateway on port ${PORT}`);
 }
 
 start();
